3c41d490b73cab48cc113d36dab1bf549007780e
Mike Storey <mike@ssgpp.com>
2023-10-16 19:51:14 -0400

    container tagging updates

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,3 @@
-docker build . --tag institute-mongosh
+docker build . --tag ghcr.io/agile-learning-institute/institute-mongosh:latest
+# docker tag institute-mongosh:latest ghcr.io/agile-learning-institute/institute-mongosh:latest
+# docker push ghcr.io/agile-learning-institute/institute-mongosh:latest
