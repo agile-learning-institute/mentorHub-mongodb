e549b113b0441f754d674688adf0d8b3bdd8cbee
Mike Storey <mike@ssgpp.com>
2023-11-02 16:44:23 -0400

    moved compose to institute repo

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,2 +1,16 @@
+# Build Docker image
 docker build . --tag ghcr.io/agile-learning-institute/institute-mongosh:latest
-# docker push ghcr.io/agile-learning-institute/institute-mongosh:latest
+if [ $? -ne 0 ]; then
+    echo "Docker build failed"
+    exit 1
+fi
+
+# Push Docker image
+if [ $1 -eq '--push' ]; then
+    docker push ghcr.io/agile-learning-institute/institute-mongosh:latest
+    if [ $? -ne 0 ]; then
+        echo "Docker Push failed"
+        exit 1
+    fi
+    echo "image pushed"
+fi
--- src/docker/run-local-db.sh
+++
@@ -1,5 +0,0 @@
-mkdir db
-cd db
-curl https://raw.githubusercontent.com/agile-learning-institute/institute-mongodb/main/src/docker/docker-compose.yaml > docker-compose.yaml
-docker compose up --detach
-cd ..
