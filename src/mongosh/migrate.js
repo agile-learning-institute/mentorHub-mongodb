95155ee3f730da680496d9ecce2864d3877f4c86
Mallory O'Donnell <mallory.odonnell0723@gmail.com>
2024-02-12 15:52:51 -0500

    Changes to schemas and data and migrate.js

--- src/mongosh/migrate.js
+++ src/mongosh/migrate.js
@@ -38,14 +38,14 @@ function checkAndCreateConstraints(
     db[collectionName].insertOne({ name: "VERSION", version: initialVersion });
     console.log("\tVersion Set");
 
-```
+
     // Load Test Data
        if (loadTest === true) {
       console.log("\tLoading Test Data");
       const result = db
         .getCollection(collectionName)
         .insertMany(
-          EJSON.deserialize(JSON.parse(fs.readFileSync(dataFile, "utf-8"))) //
+          EJSON.deserialize(JSON.parse(fs.readFileSync(dataFile, "utf-8"))) 
         );
       const count = Object.keys(result.insertedIds).length;
       console.log("\t", count, " Documents Inserted");
@@ -54,7 +54,7 @@ function checkAndCreateConstraints(
 } 
 
 
-```
+
 // Function to check and upgrade constraints
 function checkAndUpgradeConstraints(collectionName, targetVersion) {
   var versionDoc = db[collectionName].findOne({
