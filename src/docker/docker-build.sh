ce204a14598c5716ff827084c1882b51ced57c30
Mike Storey <mike@ssgpp.com>
2024-02-18 12:54:21 -0500

    docker build safety improvement

--- src/docker/docker-build.sh
+++ src/docker/docker-build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Build Docker Image
+echo "Ensure we are running in the proper folder"
+if !([[ -d "./src/docker" ]] && [[ -d "./src/mongosh" ]]); then 
+    echo "This script must be run from the repository root folder"
+    exit 1
+fi
+
+echo "Building Docker Image"
 docker build --file src/docker/Dockerfile --tag ghcr.io/agile-learning-institute/mentorhub-mongosh:latest .
 if [ $? -ne 0 ]; then
     echo "Docker build failed"
@@ -8,4 +14,4 @@ if [ $? -ne 0 ]; then
 fi
 
 # Run the Database, API, and UI containers
-/bin/bash <(curl -o - https://raw.githubusercontent.com/agile-learning-institute/mentorhub/main/docker-configurations/run-local.sh) db
+mh up mongodb
