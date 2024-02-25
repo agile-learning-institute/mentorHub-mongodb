2cb98cdd08bfd3371a1cd5e73191eb0b1b0b4871
Mike Storey <mike@agile-learning.institute>
2024-02-25 17:11:14 -0500

    refactor enums

--- src/mongosh/modules/applySchema.js
+++ src/mongosh/modules/applySchema.js
@@ -1,6 +1,7 @@
 // Function to check and create constraints (if no VERSION document exists)
 function applySchema(config) {
   const schemaFile = "./schemas/" + config.name + "-" + config.version + ".json";
+  const enumeratorsFile = "./data/enumerators.json";
 
   // Check to see if a Version document exits
   var versionDoc = db[config.name].findOne({ name: "VERSION" });
@@ -13,8 +14,21 @@ function applySchema(config) {
     db[config.name].createIndex({ name: 1 }, { unique: true });
     console.log("-Name Index created");
 
-    // Configure schema validation
+    // Add Enumerators to Schema
     const schema = require(schemaFile);
+    if (config.name !== "enumerators") {
+      const enumerators = require(enumeratorsFile);
+      Object.keys(enumerators[0][config.name]).forEach(attribute => {
+        const keys = Object.keys(enumerators[0][config.name][attribute]);
+        const type = schema.properties[attribute].bsonType;
+        var element = schema.properties[attribute];
+        if (type === "array") {element = element.items;}
+        console.log("-Enumerators Loading ", attribute);
+        element.enum = keys;
+      })
+    }
+
+    // Configure schema validation
     db.runCommand({
       collMod: config.name,
       validator: { $jsonSchema: schema },
