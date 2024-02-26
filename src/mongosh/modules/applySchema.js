767ff1cce30149739905645f211fef5e1c86ee42
Mike Storey <mike@agile-learning.institute>
2024-02-26 12:46:05 -0500

    a little more dusting

--- src/mongosh/modules/applySchema.js
+++ src/mongosh/modules/applySchema.js
@@ -8,21 +8,23 @@ function applySchema(config) {
     return;
   }
     
-  console.log("-Version Not Found, Configuring:", config.name);
-    
+  // Insert version document
+  db[config.name].insertOne({ name: "VERSION", version: config.version });
+  console.log("-Version Set", config.version);
+
   // create name index
   db[config.name].createIndex({ name: 1 }, { unique: true });
   console.log("-Name Index created");
   
   // Skip Enumerators Config of Enumerators collection 
-  const schema = require("./schemas/" + config.name + "-" + config.version + ".json");
   if (config.name === "enumerators") {
     console.log("-Enumerators - Skipping enumerators load");
     return;
   }
-
+  
   // Configure Schema Enumerators
   console.log("-Loading Enumerators");
+  const schema = require("./schemas/" + config.name + "-" + config.version + ".json");
   const enumerators = require("./data/enumerators.json");
   Object.keys(enumerators[0][config.name]).forEach(attribute => {
     const keys = Object.keys(enumerators[0][config.name][attribute]);
@@ -44,8 +46,4 @@ function applySchema(config) {
     validator: { $jsonSchema: schema },
   });
   console.log("- Schema Configured");
-
-  // Insert version document
-  db[config.name].insertOne({ name: "VERSION", version: config.version });
-  console.log("-Version Set", config.version);
 }
