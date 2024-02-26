df3cfefb2399611bf99dc9640e3ee0b31e22435f
Mike Storey <mike@agile-learning.institute>
2024-02-26 12:28:55 -0500

    a last little dusting

--- src/mongosh/modules/applySchema.js
+++ src/mongosh/modules/applySchema.js
@@ -14,25 +14,29 @@ function applySchema(config) {
   db[config.name].createIndex({ name: 1 }, { unique: true });
   console.log("-Name Index created");
   
-  // Add Enumerators to Schema
+  // Skip Enumerators Config of Enumerators collection 
   const schema = require("./schemas/" + config.name + "-" + config.version + ".json");
-  const enumerators = require("./data/enumerators.json");
-  if (config.name !== "enumerators") {
-    Object.keys(enumerators[0][config.name]).forEach(attribute => {
-      const keys = Object.keys(enumerators[0][config.name][attribute]);
-      const type = schema.properties[attribute].bsonType;
-      var element = schema.properties[attribute];
-      if (type === "array") {element = element.items;}
-      console.log("-Enumerators Loading ", attribute);
-      element.enum = keys;
-    })
-    
-    // Add last saved breadcrumb to schema
-    console.log("-Adding Breadcrumb")
-    const breadcrumb = require("./schemas/breadcrumb.json");
-    schema.properties["lastSaved"] = breadcrumb;
+  if (config.name === "enumerators") {
+    console.log("-Enumerators - Skipping enumerators load");
+    return;
   }
 
+  // Configure Schema Enumerators
+  console.log("-Loading Enumerators");
+  const enumerators = require("./data/enumerators.json");
+  Object.keys(enumerators[0][config.name]).forEach(attribute => {
+    const keys = Object.keys(enumerators[0][config.name][attribute]);
+    const type = schema.properties[attribute].bsonType;
+    var element = schema.properties[attribute];
+    if (type === "array") {element = element.items;}
+    console.log("-Enumerators Loading ", attribute);
+    element.enum = keys;
+  })
+  
+  // Add last saved breadcrumb to schema
+  console.log("-Adding Breadcrumb")
+  const breadcrumb = require("./schemas/breadcrumb.json");
+  schema.properties["lastSaved"] = breadcrumb;
 
   // Configure schema validation
   db.runCommand({
